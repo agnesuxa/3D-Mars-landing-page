d8a3cc7f4a24bca9c3cfbb02e546b7dc7b897225
agnesuxa <agnygury@yahoo.com>
2024-10-03 01:57:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let scene = new THREE.Scene();
 let aspect = window.innerWidth / window.innerHeight;
 let camera = new THREE.PerspectiveCamera(45, aspect, 0.1, 1500);
 let cameraRotation = 0;
-let cameraRotationSpeed = 0.001;
+let cameraRotationSpeed = 0.010;
 let cameraAutoRotation = true;
 let orbitControls = new THREE.OrbitControls(camera);
 
